eb87457f383fced74e73ce312e14b7c8c59283b4
Ethan Zhou <ethanez713@gmail.com>
2017-04-19 21:00:14 -0400

    clean up some lines

--- NN.cpp
+++ NN.cpp
@@ -2,10 +2,16 @@
 
 using namespace std;
 
-NN::NN(double learningRate, int numTrainInputs, int numTestInputs, int numOutputs, int mapSize, int **trainInputs, int **testInputs, int *trainTargets, int *testTargets, int maxEpochs):num_train_inputs(numTrainInputs), num_test_inputs(numTestInputs), num_outputs(numOutputs), map_size(mapSize), learning_rate(learningRate), train_inputs(trainInputs), test_inputs(testInputs), train_targets(trainTargets), test_targets(testTargets), max_epochs(maxEpochs)
-{
-    this->outputs = new output[map_size];
-
+NN::NN(double learningRate, int numTrainInputs, int numTestInputs,
+       int numOutputs, int mapSize, int **trainInputs,
+       int **testInputs, int *trainTargets, int *testTargets,
+       int maxEpochs) :
+  num_train_inputs(numTrainInputs), num_test_inputs(numTestInputs),
+  num_outputs(numOutputs), map_size(mapSize),
+  learning_rate(learningRate), train_inputs(trainInputs),
+  test_inputs(testInputs), train_targets(trainTargets),
+  test_targets(testTargets), max_epochs(maxEpochs) {
+  this->outputs = new output[map_size];
 }
 
 
--- NN.h
+++ NN.h
@@ -1,14 +1,7 @@
 #ifndef __NN_h
 #define __NN_h
 
-#include <iostream>
 #include <vector>
-#include <string>
-#include <cstdlib>
-#include <fstream>
-#include <stdio.h>
-#include <sstream>
-#include <cmath>
 
 using namespace std;
 
@@ -22,13 +15,13 @@ public:
     NN(double learningRate, int numTrainInputs, int numTestInputs, int numOutputs, int mapSize, int **trainInputs, int **testInputs, int *trainTargets, int *testTargets, int maxEpochs);
     void test();
     void train();
-    
+
 private:
     int num_train_inputs;
     int num_test_inputs;
     int num_outputs;
     int map_size;
-    
+
     double learning_rate;
     int **train_inputs;
     int **test_inputs;
@@ -36,7 +29,7 @@ private:
     int *test_targets;
     output *outputs;
     int max_epochs;
-    
+
 };
 
 #endif
