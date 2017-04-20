6ba1055db12d4e5f0aa7dd21c5e263d1815e66f2
Jake Adicoff <jakeadicoff@MacBook-Pro-2.local>
2017-04-19 22:38:59 -0400

    some more code

--- NN.h
+++ NN.h
@@ -2,7 +2,10 @@
 #define __NN_h
 
 #include <vector>
-
+#include <random>
+#include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
 
 struct output {
@@ -12,19 +15,19 @@ struct output {
 
 class NN {
 public:
-    NN(double learningRate, int numTrainInputs, int numTestInputs,
-       int numOutputs, int mapSize, vector<vector<int> > trainInputs,
-       vector<vector<int> > testInputs, vector<int> trainTargets,
-       vector<int> testTargets, int maxEpochs);
-    void test();
-    void train();
-
-private:
+  NN(double learningRate, int numTrainInputs, int numTestInputs,
+     int numOutputs, int mapSize, vector<vector<int> > trainInputs,
+     vector<vector<int> > testInputs, vector<int> trainTargets,
+     vector<int> testTargets, int maxEpochs);
+  void test();
+  void train();
+  
+ private:
     int num_train_inputs;
     int num_test_inputs;
     int num_outputs;
     int map_size;
-
+    
     double learning_rate;
     vector<vector<int>> train_inputs;
     vector<vector<int>> test_inputs;
@@ -32,7 +35,12 @@ private:
     vector<int> test_targets;
     vector<output> outputs;
     int max_epochs;
-
+    
+    void initialize_weights();
+    void update_weights(int output_index, double g,double g_prime,double target);
+    double activation_function(double x);
+    double ddx_activation_function(double x);
+    
 };
 
 #endif
