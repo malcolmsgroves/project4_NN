629f721bed4a0e535e3bf719df237cf1af59e0d2
Malcolm Groves <macgroves@gmail.com>
2017-04-22 15:00:36 -0400

    Debugging, added functionality for 10 output nodes

--- NN.cpp
+++ NN.cpp
@@ -2,30 +2,33 @@
 
 using namespace std;
 
+
+
 NN::NN(double learningRate, int numTrainInputs, int numTestInputs,
        int numOutputs, int mapSize, vector<vector<int> > trainInputs,
        vector<vector<int> > testInputs, vector<int> trainTargets,
        vector<int> testTargets, int maxEpochs):
-  num_train_inputs(numTrainInputs), num_test_inputs(numTestInputs),
-  num_outputs(numOutputs), map_size(mapSize),
-  learning_rate(learningRate), train_inputs(trainInputs),
-  test_inputs(testInputs), train_targets(trainTargets),
-  test_targets(testTargets), max_epochs(maxEpochs) {
-
-  srand(time(NULL));
-  initialize_weights();
+num_train_inputs(numTrainInputs), num_test_inputs(numTestInputs),
+num_outputs(numOutputs), map_size(mapSize),
+learning_rate(learningRate), train_inputs(trainInputs),
+test_inputs(testInputs), train_targets(trainTargets),
+test_targets(testTargets), max_epochs(maxEpochs) {
+    
+    srand(time(NULL));
+    initialize_weights();
 }
 
 void NN::initialize_weights() {
-  //outputs.reserve(num_outputs);
-  for(int i = 0; i < num_outputs; i++) {
-    output o;
-    outputs.push_back(o);
-    for(int j = 0; j < map_size; j++) {
-      outputs[i].weights.push_back(2*double(rand())/double(RAND_MAX) -1);
+    //outputs.reserve(num_outputs);
+    for(int i = 0; i < num_outputs; i++) {
+        output o;
+        outputs.push_back(o);
+        for(int j = 0; j < map_size; j++) {
+            outputs[i].weights.push_back(0.3*double(rand())/double(RAND_MAX) - 0.15);
+            //outputs[i].weights.push_back(0.15);
+        }
+        outputs[i].weights.push_back(1);
     }
-    outputs[i].weights.push_back(1);
-  }
 }
 
 //void NN::test() {
@@ -33,83 +36,123 @@ void NN::initialize_weights() {
 //}
 //
 void NN::train() {
-  cout << "in train" << endl;
-  for(int i = 0; i < max_epochs; i++) {
-    for(int input = 0; input < num_train_inputs; input++) { // omg, change this you dip
-      double target = train_targets[input];
-      for(int output = 0; output < num_outputs; output++) {
- 	double dot_product = 0;
- 	for(int weight_index = 0; weight_index < map_size+1; weight_index++) {
-	  if(weight_index < map_size) {
-	    dot_product += train_inputs[input][weight_index]*outputs[output].weights[weight_index];
-	  } else {
-	    dot_product += outputs[output].weights[weight_index]; // bias node
-	  }
-	}
- 	double g = activation_function(dot_product);
- 	double g_prime = ddx_activation_function(dot_product);
-	cout << dot_product << endl;
-	cout << "g = " << g << endl;
-	cout << "g' = " << g_prime << endl;
- 	update_weights(output, input, g, g_prime, target);
-      }
+    cout << "in train" << endl;
+    for(int i = 0; i < max_epochs; i++) {
+        for(int input = 0; input < num_train_inputs; input++) { // omg, change this you dip
+            double target = train_targets[input];
+            for(int output = 0; output < num_outputs; output++) {
+                double dot_product = 0;
+                for(int weight_index = 0; weight_index <= map_size; weight_index++) {
+                    if(weight_index < map_size) {
+                        dot_product += train_inputs[input][weight_index]*outputs[output].weights[weight_index];
+                    } else {
+                        dot_product += outputs[output].weights[weight_index]; // bias node
+                    }
+                }
+                double g = activation_function(dot_product);
+                double g_prime = ddx_activation_function(dot_product);
+                //cout << dot_product << endl;
+                //cout << "g = " << g << endl;
+                //cout << "g' = " << g_prime << endl;
+                //cout << "g x 10 = " << int(g*10) << endl;
+                update_weights(output, input, g, g_prime, target);
+            }
+        }
+        test();
     }
-  }
 }
 
 void NN::update_weights(int output_index, int input_index, double g, double g_prime, double target) {
-  double output_value;
-  if(num_outputs > 1) {
-    output_value = g;
-    if(output_index != target) {
-      target = 0; // if index does not match target digit
-    } else { 
-      target = 1; // if index matches target 
-    }
-  } else {
-    // no need to set target, as its already set
-    output_value = int ((g*10)+.5); // omg, CHECK THIS 
-  }
-  for(int i = 0; i < map_size+1; i++) {
-    if(i < map_size) {
-      outputs[output_index].weights[i] += learning_rate*(target-output_value)*g_prime*train_inputs[input_index][i];
+    //double output_value;
+    if(num_outputs > 1) {
+        //output_value = g;
+        if(output_index != target) {
+            target = 0.0; // if index does not match target digit
+        } else {
+            target = 1.0; // if index matches target
+        }
     } else {
-      outputs[output_index].weights[i] += learning_rate*(target-output_value)*g_prime; // bias node
+        // no need to set target, as its already set
+        //output_value = int ((g*10)); // got rid of + 0.5
+        target = target / 10;
     }
-  }
+    for(int i = 0; i < map_size+1; i++) {
+        if(i < map_size) {
+            outputs[output_index].weights[i] += learning_rate * (target - g) * g_prime * train_inputs[input_index][i];
+        } else {
+            outputs[output_index].weights[i] += learning_rate * (target - g) * g_prime; // bias node
+        }
+    }
 }
 
+double spread = 0.01;
+
 double NN::activation_function(double x) {
-  return 1/(1+exp(.5-x));
+    return 1/(1+exp(.5-x));
 }
 
 double NN::ddx_activation_function(double x) {
-  return (1.64872*exp(x))/pow((1.64872+exp(x)),2);
+    return (1.64872*exp(x))/pow((1.64872+exp(x)),2);
 }
 
 
 // tbh worte this on 4/20, so check for bugs
 void NN::test() {
-  int num_correct = 0;
-  for(int input = 0; input < num_test_inputs; input++) {
-    int target = test_targets[input];
-    for(int output = 0; output < num_outputs; output++) {
-      double dot_product = 0;
-      for(int weight_index = 0; weight_index < map_size+1; weight_index++) {
-	if(weight_index < map_size) {
-	  dot_product += test_inputs[input][weight_index]*outputs[output].weights[weight_index];
-	} else {
-	  dot_product += outputs[output].weights[weight_index];
-	}
-      }
-      //cout << dot_product << endl;
-      if(num_outputs == 1) {
-	if(target == int(10*activation_function(dot_product))) {
-	  num_correct++;
-	}
-      }
+    int num_correct = 0;
+    
+    // for every test image
+    for(int input = 0; input < num_test_inputs; input++) {
+        
+        int target = test_targets[input];
+        
+        double answer_value = -1;
+        int answer = 0;
+        
+        // calculate the output at every node
+        for(int output = 0; output < num_outputs; output++) {
+            double dot_product = 0;
+            
+            // for every weight connected to the output
+            for(int weight_index = 0; weight_index <= map_size; weight_index++) {
+                
+                // real inputs
+                if(weight_index < map_size) {
+                    dot_product += test_inputs[input][weight_index]*outputs[output].weights[weight_index];
+                }
+                //bias node
+                else {
+                    dot_product += outputs[output].weights[weight_index];
+                }
+            }
+            //cout << dot_product << endl;
+            double g = activation_function(dot_product);
+            
+            if(num_outputs == 1) {
+                cout << int(10*g) << endl;
+                cout << target << endl << endl;
+                if(target == int(10*g)) {
+                    num_correct++;
+                }
+                   
+            }
+            
+            
+            else if(g > answer_value) {
+                
+                answer = output;
+                answer_value = g;
+            }
+        }
+        if(num_outputs == 10) {
+            cout << "Answer = " << answer << endl;
+            cout << "Target = " << target << endl << endl;
+            if(answer == target) {
+                ++num_correct;
+            }
+        }
+                   
+                   
     }
-  }
-  cout << "number of correct tests " << num_correct << endl;
-  cout << "number of tests " << num_test_inputs << endl;
+    cout << "number of correct tests " << num_correct << endl;
+    //cout << "number of tests " << num_test_inputs << endl;
 }
--- main.cpp
+++ main.cpp
@@ -9,20 +9,20 @@ int main(int argc, char** argv) {
     double learningRate = stod(argv[3]);
     int outputNodes = stoi(argv[4]);
     
-
-
-
+    
+    
+    int max_epochs = 50;
     
     Problem digit_prob = read_file("optdigits.tra");
     cout << digit_prob.map_size << endl;
     cout << digit_prob.num_inputs << " " << digit_prob.inputs.size() << endl;
     for(int i = 0; i < digit_prob.num_inputs; ++i) {
-      //cout << digit_prob.inputs[10][i] << ", ";
+        //cout << digit_prob.inputs[10][i] << ", ";
     }
     //cout << digit_prob.targets[10];
     cout << "hey " << endl;
     cout << digit_prob.inputs[0].size() << endl;
-    NN net(learningRate,digit_prob.num_inputs,digit_prob.num_inputs,outputNodes,digit_prob.map_size, digit_prob.inputs,digit_prob.inputs,digit_prob.targets,digit_prob.targets,50);
+    NN net(learningRate,digit_prob.num_inputs,digit_prob.num_inputs,outputNodes,digit_prob.map_size, digit_prob.inputs,digit_prob.inputs,digit_prob.targets,digit_prob.targets,max_epochs);
     net.train();
     net.test();
 }
@@ -31,62 +31,62 @@ int main(int argc, char** argv) {
 // reads in sets of digit recognition problems from files
 Problem read_file(string problem_file_name) {
     
-  Problem digit_recognition;
-  digit_recognition.map_size = 0;
-  digit_recognition.num_inputs = 0;
-  vector<int> dummy_inputs;
-  digit_recognition.inputs.push_back(dummy_inputs);
-  
-  string curr_number = "";
-  char curr_char;
-  
-  ifstream problem_stream;
-  problem_stream.open(problem_file_name.c_str(), ios::in);
-  
-  if(!problem_stream.good()) {
-    cout << "Error: not able to open file" << endl;
-  }
-  
-  // read the first line and determine map size
-  while(problem_stream.peek() != '\n') { 
-    problem_stream.get(curr_char);
+    Problem digit_recognition;
+    digit_recognition.map_size = 0;
+    digit_recognition.num_inputs = 0;
+    vector<int> dummy_inputs;
+    digit_recognition.inputs.push_back(dummy_inputs);
     
-    // if comma, save number and move on
-    if(curr_char == ',') {
-      digit_recognition.inputs[digit_recognition.num_inputs].push_back(stoi(curr_number));
-      curr_number.clear();
-      ++digit_recognition.map_size;
+    string curr_number = "";
+    char curr_char;
+    
+    ifstream problem_stream;
+    problem_stream.open(problem_file_name.c_str(), ios::in);
+    
+    if(!problem_stream.good()) {
+        cout << "Error: not able to open file" << endl;
     }
     
-    // else, add char to curr number string
-    else {
-      curr_number += curr_char;
-    }
-  }
-  
-  while(problem_stream.peek() != EOF) {
-    
-    problem_stream.get(curr_char);
-    
-    // if comma, save number and move on
-    if(curr_char == ',') {
-      digit_recognition.inputs[digit_recognition.num_inputs].push_back(stoi(curr_number));
-      curr_number.clear();
-    }
-    
-    // if new line, cap off current digit image and move on to next one
-    else if(curr_char == '\n') {
-      digit_recognition.targets.push_back(stoi(curr_number));
-      curr_number.clear();
-      ++digit_recognition.num_inputs;
-      //cout << digit_recognition.num_inputs << endl;
-      digit_recognition.inputs.push_back(dummy_inputs);
+    // read the first line and determine map size
+    while(problem_stream.peek() != '\n') {
+        problem_stream.get(curr_char);
+        
+        // if comma, save number and move on
+        if(curr_char == ',') {
+            digit_recognition.inputs[digit_recognition.num_inputs].push_back(stoi(curr_number));
+            curr_number.clear();
+            ++digit_recognition.map_size;
+        }
+        
+        // else, add char to curr number string
+        else {
+            curr_number += curr_char;
+        }
     }
     
-    // else, add char to curr number string
-    else {
-      curr_number += curr_char;
+    while(problem_stream.peek() != EOF) {
+        
+        problem_stream.get(curr_char);
+        
+        // if comma, save number and move on
+        if(curr_char == ',') {
+            digit_recognition.inputs[digit_recognition.num_inputs].push_back(stoi(curr_number));
+            curr_number.clear();
+        }
+        
+        // if new line, cap off current digit image and move on to next one
+        else if(curr_char == '\n') {
+            digit_recognition.targets.push_back(stoi(curr_number));
+            curr_number.clear();
+            ++digit_recognition.num_inputs;
+            //cout << digit_recognition.num_inputs << endl;
+            digit_recognition.inputs.push_back(dummy_inputs);
+        }
+        
+        // else, add char to curr number string
+        else {
+            curr_number += curr_char;
+        }
     }
-  }
-  return digit_recognition;  
+    return digit_recognition;  
 }
